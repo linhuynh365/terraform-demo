06c5c70fc6b186d8c05a3299a1904dfaeea95924
linhuynh365 <lin.huynh@concordusa.com>
2024-01-09 10:54:09 -0600

    feature: change resource name

--- demo1/main.tf
+++ demo1/main.tf
@@ -11,19 +11,18 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  #   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "example-resource-1"
+  name     = "example-resource"
   location = "centralus"
 }
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network-1"
+  name                = "example-network"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
--- demo2/platform_resources/terraform.tfvars
+++ demo2/platform_resources/terraform.tfvars
@@ -1,5 +1,5 @@
 location             = "centralus"
-resource_group_name  = "example-resources"
-virtual_network_name = "example-network"
+resource_group_name  = "example-resources-1"
+virtual_network_name = "example-network-1"
 vnet_address_space   = ["10.0.0.0/16"]
 
